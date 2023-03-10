42ac1d6fafeb4c3aee48d9b320e8ca325a9e4b5f
Chloe Lin <clin20@unc.edu>
2023-03-10 13:59:09 -0500

    Remove extraneous loop, handle directions

--- cli.js
+++ cli.js
@@ -1,48 +1,60 @@
 #!/usr/bin/env node
-const [,, ...args] = process.argv;
 const fs = require("fs");
 
 const moment = require("moment-timezone")
 const timezone = moment.tz.guess()
+const args = minimist(process.argv.slice()); 
 
-let i = 0;
-let lat = "";
-let long = "";
+let lat;
+let long;
 let day = 0;
-let ti = timezone;
+let tz;
 
-while (i < args.length) {
-	if(args[i] == "-h") {
-		//Show this help message and exit
-		fs.readFile('./helpFile.txt', 'utf8', (err, data) => {
-			if (err) {
-			  console.error(err);
-			  return;
-			}
-			console.log(data);
-		  });
-	} else if (args[i] == "-n") {
-		//positive latitude, north
-		lat = i + 1;
-	} else if (args[i] == "-s") {
-		//negative latitude, south
-		lat = i + 1;
-	} else if (args[i] == "-e") {
-		//positive longitude, east
-		long = i + 1;
-	} else if (args[i] == "-w") {
-		//negative longitude, west
-		long = i + 1;
-	} else if (args[i] == "-z") {
-		//Time zone: uses tz.guess() from moment-timezone by default.
-		ti = i + 1;
-	} else if (args[i] == "-d") {
-		//Day to retrieve weather: 0 is today; defaults to 1.
-		day = i + 1;
-	} else if (args[i] == "-j") {
-		//Echo pretty JSON from open-meteo API and exit.
-	}
-	i++;
+
+if("h" in args) {
+	//Show this help message and exit
+	fs.readFile('./helpFile.txt', 'utf8', (err, data) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+		console.log(data);
+	});
+	process.exit(0);
+}
+
+//handles latitude
+if (args.n) {
+    lat = args.n;
+} else if (args.s) {
+    lat = args.s * -1;
+} else {
+    console.log("Latitude of range");
+    process.exit(0);
+}
+
+//handles longitude
+if ("e" in args) {
+    long = args["e"];
+} else if ("w" in args) {
+    long = args["w"] * -1;
+} else {
+    console.log("Longitude out of range");
+    process.exit(0);
+}
+
+if (args[i] == "-z") {
+	//Time zone: uses tz.guess() from moment-timezone by default.
+	ti = i + 1;
+}
+
+if (args[i] == "-d") {
+	//Day to retrieve weather: 0 is today; defaults to 1.
+	day = i + 1;
+}
+
+if (args[i] == "-j") {
+	//Echo pretty JSON from open-meteo API and exit.
 }
 
 const response = fetch('https://api.open-meteo.com/v1/forecast?latitude=' + lat + '&longitude=' + long + '&daily=precipitation_hours&timezone=' + tz);
